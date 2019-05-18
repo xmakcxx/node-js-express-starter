e23631a7973f7d7fe30335bf96c52228d369a610
Alexander Makalov <amakalau@exadel.com>
2019-05-18 15:37:22 +0300

    feat(init): add base structure

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/tasks', (req, res) => {
+  const tasks = [
+    {
+      _id: Math.random(),
+      name: Math.random()
+    }
+  ];
+  const { query } = req;
+  tasks.push({
+    _id: Math.random(),
+    name: query.name,
+    age: query.age
+  });
+  res.send(JSON.stringify(tasks));
+});
+
+app.get('/task/2.1', (req, res) => {
+  const result = { info: 'text' };
+  res.send(JSON.stringify(result));
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
